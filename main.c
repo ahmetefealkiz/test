4af9c44a3ac34befc3ed0e3c32b11a4630eff402
Ahmet Efe Alkız <aalkiz@k1m32s08.42istanbul.com.tr>
2025-07-16 15:29:14 +0300

    asdasd

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello, World!\n");
+    printf("This project uses the C17 language standard version.\n");
+    return 0;
+}
